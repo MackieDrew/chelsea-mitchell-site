5f3b14533cddcc1d9b1913b15f917907e8473ee1
Mackenzie Drew <mackiedrew@gmail.com>
2017-02-03 21:32:52 -0500

    Blanked project for recreation

--- README.md
+++
@@ -1,3 +0,0 @@
-## Introduction
-
-The site is being created for Chelsea Mitchell with her permission to publish the source.
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './javascript/components/App'
-import './scss/index.scss'
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
