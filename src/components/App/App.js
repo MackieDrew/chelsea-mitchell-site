ed416a703294a7e0e9afeda09520027ecd20c164
Mackenzie Drew <mackiedrew@gmail.com>
2017-02-26 22:01:20 -0500

    Another overhaul, added breakpoints,

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,7 +24,6 @@ class App extends React.Component {
     return (
       <div className="app">
         <Landing />
-        <Content />
       </div>
     )
   }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import './Landing.scss'
-import { Grid, Row, Col } from 'react-bootstrap'
 import logo from '../../images/logo.png'
 
-const Landing = () =>
-  <Grid fluid className="landing">
-    <Row className="landing__growing-portion visible-lg">
-      <Col>
-        <img alt="Chelsea Mitchell Logo" className='logo' src={logo}></img>
-      </Col>
-    </Row>
-    
-    <Row className="landing__growing-portion-mobile hidden-lg">
-      <Col>
-        <img alt="Chelsea Mitchell Logo" className='logo' src={logo}></img>
-      </Col>
-    </Row>
 
-  </Grid>
+
+
+
+const Landing = () =>
+  <div fluid className="container-fluid landing">
+    <div className="row landing__growing-portion">
+      <div className="col">
+        <img alt="Chelsea Mitchell Logo" className='logo' src={logo}></img>
+      </div>
+    </div>
+
+  </div>
 
 export default Landing
